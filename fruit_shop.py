e036ca6b51bf4a7f6bfcde6abfb1fcda37e0cbe9
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2020-08-28 15:58:04 -0500

    Create fruit_shop.py

---
+++ fruit_shop.py
@@ -0,0 +1,17 @@
+def main ():
+    fruit_list = ['apple', 'mango', 'orange', 'banana']
+    '''
+    Write your code below.
+    Your code should ask the user for a fruit, and then add it to the fruit_list
+    Then, you shall print the fruit_list with a new fruit
+    '''
+    # Type code here
+
+    '''
+    Optional:
+    The function fruit_count() returns a dictionary with the inventory left for certain fruits.
+    Add the fruit 'durian' as a key to fruit_dic and set its value as 3.
+    Then print the dictionary.
+    '''
+    fruit_dic = {'apple' : 2 , 'mango' : 54, 'orange' : 43, 'banana' : 21}
+    
